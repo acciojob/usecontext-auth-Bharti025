aa143a1f2bfef952ec70d00b2f13a47d0a0d8ac2
Bharti <bhartiwadhwa025@gmail.com>
2025-10-16 20:12:05 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 
-import React from "react";
+import React,{useState,useContext,createContext} from "react";
 import './../styles/App.css';
 
+const AuthContext = createContext();
+
 const App = () => {
+  const [user,setUser]=useState("You are not authenticated");
+  const handleClick=()=>{
+    setUser("You are now authenticated, you can proceed")
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+   <AuthContext.Provider value={user}>
+     <h1>Click on checkbox to get authenticated</h1>
+     <p>{user}</p>
+     <input type="checkbox" id="authCheckbox" onClick={handleClick}/>
+     <label htmlFor="authCheckbox">I'm not a robot</label>
+   </AuthContext.Provider>
   )
 }
 
